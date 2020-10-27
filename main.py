6e285fc16442721c04f2668c414c081852e1090b
Malik <khada005@umn.edu>
2020-10-27 09:24:04 -0500

    added main menu

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import site_management
 import numpy
 import settings
 import get_time
+import menu
 
 #print(numpy.percentile([1,12,3,5,31], settings.percentile_grain))
 
@@ -20,3 +21,13 @@ t = get_time.range_time()
 
 print(get_time.time_codify(week, t))
 '''
+
+main_menu_options = ["Analyze site", "Site management", "Quit"]
+main_menu = menu.select_element("Main menu", main_menu_options)
+
+while main_menu != "Quit":
+    if main_menu == "Analyze site":
+        sample_analysis.collect_samples()
+    elif main_menu == "Site management":
+        site_management.interface()
+    main_menu = menu.select_element("Main menu", main_menu_options)
--- site_management.py
+++ site_management.py
@@ -4,6 +4,15 @@ import menu
 import os
 import settings
 
+def interface():
+  choice = menu.select_element("Site management", ["Add site", "Remove site", "Exit"])
+  while choice != "Exit":
+    if choice == "Add site":
+      add_site()
+    elif choice == "Remove site":
+      remove_site()
+    choice = menu.select_element("Site management", ["Add site", "Remove site", "Exit"])
+
 def add_site():
   '''Add a site to site_data if it's in auto_sites.txt'''
   site = menu.select_integer("site number", 0, 0, False)
@@ -17,7 +26,10 @@ def add_site():
 
 def remove_site():
   '''Remove a site from site_data'''
-  site = menu.select_element("site", listdir("site_data"))
+  site = menu.select_element("site", listdir("site_data") + ["Exit"])
+  if site == "Exit":
+    return
+  
   file_location = "site_data/" + site
 
   os.remove(file_location)
